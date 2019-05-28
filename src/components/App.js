c39796742f3114fe21da45cc9481d24546eaedd6
Lili Boxer <ninaliboxer@gmail.com>
2019-05-28 16:07:08 -0700

    index js code in

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import Component from './Component.js';
+
+class App extends Component {
+    render() {
+        
+    }
+
+    renderTemplate() {
+        return /*html*/ `
+            <div>
+                <main></main>
+            </div>
+        `;
+    }
+}
+
+export default App;
